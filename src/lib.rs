3b4e8e15a86afa92620701175d03a5e7da95ce79
Jonathan <jonthanwoollettlight@gmail.com>
2021-08-17 17:12:36 +0100

    reset

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,27 @@
 //! A crate to allow converting generic primitives into bytes.
 //!
-//! Defines `TryFromBytes` and `IntoBytes` traits and implements them on all numerical primitives.
+//! Defines `FromBytes` and `IntoBytes` traits for converting generics into bytes.
+//!
+//! Also implements them on all numerical primitives.
 
-use std::{array::TryFromSliceError, convert::TryInto, u8};
+#![feature(const_generics)]
+#![feature(const_evaluatable_checked)]
 
-/// Defines a type can be converted from a byte slice.
+/// Defines a type can be converted from a bytes array.
 /// ```
-/// use num_bytes::TryFromBytes;
+/// use num_bytes::FromBytes;
 /// let a = [8,0,0,0];
-/// let b = u32::try_from_le_bytes(&a).unwrap();
-/// assert_eq!(b,8);
+/// let b = i32::from_le_bytes(a);
+/// assert_eq!(b, 8);
 /// ```
-pub trait TryFromBytes: Sized {
-    fn try_from_le_bytes(bytes: &[u8]) -> Result<Self, TryFromSliceError>;
-    fn try_from_be_bytes(bytes: &[u8]) -> Result<Self, TryFromSliceError>;
+/// `isize` and `usize` implementations compile to implement `FromBytes<2>`, `FromBytes<4>` or `FromBytes<8>` depending on your system.
+///
+/// It simply says `FromBytes<8>` in documentation because the machine which has compiled the documentation is 64 bit.
+pub trait FromBytes: Sized {
+    fn from_le_bytes(bytes: [u8; std::mem::size_of::<Self>()]) -> Self;
+    fn from_be_bytes(bytes: [u8; std::mem::size_of::<Self>()]) -> Self;
 }
+
 /// Defines a type can be converted into a byte vector.
 /// ```
 /// use num_bytes::IntoBytes;
@@ -22,50 +29,40 @@ pub trait TryFromBytes: Sized {
 /// let b = a.into_le_bytes();
 /// assert_eq!(b,[8,0,0,0]);
 /// ```
+/// `isize` and `usize` implementations compile to implement `FromBytes<2>`, `FromBytes<4>` or `FromBytes<8>` depending on your system.
+///
+/// It simply says `FromBytes<8>` in documentation because the machine which has compiled the documentation is 64 bit.
 pub trait IntoBytes: Sized {
-    fn into_le_bytes(self) -> Vec<u8>;
-    fn into_be_bytes(self) -> Vec<u8>;
+    type Output;
+    fn into_le_bytes(self) -> [u8; std::mem::size_of::<Self>()];
+    fn into_be_bytes(self) -> [u8; std::mem::size_of::<Self>()];
 }
-macro_rules! impl_try_from_bytes {
-    ($T: ident) => {
-        impl TryFromBytes for $T {
-            fn try_from_le_bytes(bytes: &[u8]) -> Result<Self, TryFromSliceError> {
-                Ok(Self::from_le_bytes(bytes.try_into()?))
+
+macro_rules! impl_from_bytes {
+    ($T: ty) => {
+        impl FromBytes for $T {
+            fn from_le_bytes(bytes: [u8; std::mem::size_of::<$T>()]) -> Self {
+                Self::from_le_bytes(bytes)
             }
-            fn try_from_be_bytes(bytes: &[u8]) -> Result<Self, TryFromSliceError> {
-                Ok(Self::from_be_bytes(bytes.try_into()?))
+            fn from_be_bytes(bytes: [u8; std::mem::size_of::<$T>()]) -> Self {
+                Self::from_be_bytes(bytes)
             }
         }
     };
 }
 macro_rules! impl_into_bytes {
-    ($T: ident) => {
-        impl IntoBytes for $T {
-            fn into_le_bytes(self) -> Vec<u8> {
-                Vec::from(self.to_le_bytes())
+    ($T: ty) => {
+        impl IntoBytes for $T where [u8;std::mem::size_of::<$T>()]: Sized {
+            type Output = [u8;std::mem::size_of::<$T>()];
+            fn into_le_bytes(self) -> Self::Output {
+                self.to_le_bytes()
             }
-            fn into_be_bytes(self) -> Vec<u8> {
-                Vec::from(self.to_be_bytes())
+            fn into_be_bytes(self) -> Self::Output {
+                self.to_be_bytes()
             }
         }
     };
 }
-
-// Try from implementations.
-impl_try_from_bytes!(i8);
-impl_try_from_bytes!(i16);
-impl_try_from_bytes!(i32);
-impl_try_from_bytes!(i64);
-impl_try_from_bytes!(u8);
-impl_try_from_bytes!(u16);
-impl_try_from_bytes!(u32);
-impl_try_from_bytes!(u64);
-
-impl_try_from_bytes!(f32);
-impl_try_from_bytes!(f64);
-
-impl_try_from_bytes!(isize);
-impl_try_from_bytes!(usize);
 
 // Into implementations.
 impl_into_bytes!(i8);
@@ -81,4 +78,21 @@ impl_into_bytes!(f32);
 impl_into_bytes!(f64);
 
 impl_into_bytes!(isize);
+
 impl_into_bytes!(usize);
+
+// From implementations
+impl_from_bytes!(i8);
+impl_from_bytes!(i16);
+impl_from_bytes!(i32);
+impl_from_bytes!(i64);
+impl_from_bytes!(u8);
+impl_from_bytes!(u16);
+impl_from_bytes!(u32);
+impl_from_bytes!(u64);
+
+impl_from_bytes!(f32);
+impl_from_bytes!(f64);
+
+impl_from_bytes!(isize);
+impl_from_bytes!(usize);
--- tests/into.rs
+++ tests/into.rs
@@ -1,4 +1,15 @@
 use num_bytes::IntoBytes;
+
+#[test]
+fn into_test() {
+    let a = 8isize;
+    
+    fn into<T:IntoBytes>(x:T) {
+        let arr:[u8;4] = x.into_le_bytes();
+        println!("x: {:.?}",)arr;
+    }
+}
+
 #[test]
 fn into_u8() {
     let a = 8u8;
@@ -45,5 +56,30 @@ fn into_i32() {
 fn into_i64() {
     let a = 8i64;
     let b = a.into_le_bytes();
+    assert_eq!(b, [8, 0, 0, 0, 0, 0, 0, 0]);
+}
+
+#[cfg(target_pointer_width = "64")]
+#[test]
+fn into_usize() {
+    let a = 8usize;
+    let b = a.into_le_bytes();
+    #[cfg(target_pointer_width = "16")]
+    assert_eq!(b, [8, 0]);
+    #[cfg(target_pointer_width = "32")]
+    assert_eq!(b, [8, 0, 0, 0]);
+    #[cfg(target_pointer_width = "64")]
+    assert_eq!(b, [8, 0, 0, 0, 0, 0, 0, 0]);
+}
+
+#[test]
+fn into_isize() {
+    let a = 8isize;
+    let b = a.into_le_bytes();
+    #[cfg(target_pointer_width = "16")]
+    assert_eq!(b, [8, 0]);
+    #[cfg(target_pointer_width = "32")]
+    assert_eq!(b, [8, 0, 0, 0]);
+    #[cfg(target_pointer_width = "64")]
     assert_eq!(b, [8, 0, 0, 0, 0, 0, 0, 0]);
 }
